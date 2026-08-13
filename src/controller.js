8e6a73169720abc267daaaec5b0815134c4c90d1
parthsawang <parthsawang61@gmail.com>
2026-08-13 23:46:56 +0530

    can't able to use controller here and dont no how to used

--- src/controller.js
+++
@@ -1,7 +0,0 @@
-const { StatusCodes } = require('http-status-codes');
-const octakit = require('./test-github');
-
-async function fetchData(req,res) {
-    
-    
-}
---
+++ src/controllers/github.controller.js
@@ -0,0 +1,3 @@
+const { StatusCodes } = require("http-status-codes");
+const  octokit = require("../test-github");
+
