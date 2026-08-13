8b504c18ac4c9728be58cf216d70bd2ecb1fc898
parthsawang <parthsawang61@gmail.com>
2026-08-13 23:11:17 +0530

    data fetching is complete

--- src/test-github.js
+++ src/test-github.js
@@ -1,4 +1,5 @@
 const{ Octokit} = require('@octokit/rest');
+const { StatusCodes } = require('http-status-codes');
 require('dotenv').config()
 
 // 
@@ -6,5 +7,17 @@ const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 
+async function fetchUserEvents() {
+    const response = await octokit.request(
+        'GET /users/{username}/events/public',
+        {
+            username: 'parthsawang'
+        }
+    );
 
-module.export = Octokit;
+    return response.data;
+}
+
+fetchUserEvents()
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
